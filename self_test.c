5386ab7404dd7cd4c44d742a88bf70b23ef63726
dclarino <davidbclarino@gmail.com>
2013-02-18 08:17:25 -0800

    Added memory routines and self_test

---
+++ self_test.c
@@ -0,0 +1,17 @@
+#include <p24F16KA101.h>
+#include "sensor.h"
+int test_WDT(int delay) {
+  RCONbits.SWDTEN = 1; //enable Watchdog timer
+  //set for 3 seconds
+  //go to sleep
+  sends("I can't do that Dave");//on wakeup, send display
+  return 1;
+}
+
+int test_INT2() {
+    configure_wakeup_interrupt();
+   //go to sleep
+    asm_sleep();
+    //on wake, print out
+    sends("Good Morning Dave");
+}
