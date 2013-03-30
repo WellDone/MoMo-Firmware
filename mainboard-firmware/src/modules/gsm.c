2da7a07cd756c034e731c82bcced997429fee5e2
Austin McGee <austinwm@gmail.com>
2013-03-30 15:22:14 -0700

    Update GSM code to set up UART on init, initially disabled.  Enable on gsm_on and disable again on gsm_off to save power.

--- mainboard-firmware/src/modules/gsm.c
+++ mainboard-firmware/src/modules/gsm.c
@@ -13,6 +13,20 @@ void gsm_init()
     //Set direction to out
     GSM_MODULE_ON_TRIS = 0;
     GSM_POWER_TRIS = 0;
+    gsm_configure_serial();
+    uart_set_disabled(U1, 1);
+}
+
+void gsm_configure_serial()
+{
+    uart_parameters params_uart1;
+
+    //Clock enter high-speed mode
+
+    params_uart1.baud = 115200;
+    params_uart1.hw_flowcontrol = 0;
+    params_uart1.parity = NoParity;
+    configure_uart( U1, &params_uart1 );
 }
 
 int gsm_send_at_cmd( const char* cmd )
@@ -31,26 +45,16 @@ void gsm_send_sms( const char* destination, const char* message )
     put( U1, 0x1A ); // send ctrl-z
 }
 
-void gsm_configure_serial()
-{
-    uart_parameters params_uart1;
-
-    //Clock enter high-speed mode
-
-    params_uart1.baud = 115200;
-    params_uart1.hw_flowcontrol = 0;
-    params_uart1.parity = NoParity;
-    configure_uart( U1, &params_uart1 );
-}
-
 void gsm_on()
 {
     GSM_POWER_ON();
     wait_ms( 1 );
     GSM_MODULE_ON_PIN = 0;
+    uart_set_disabled(U1, 0);
 }
 
 void gsm_off()
 {
     GSM_POWER_OFF();
+    uart_set_disabled(U1, 1);
 }
