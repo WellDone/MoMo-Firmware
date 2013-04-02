b5cbf0ec2ddc73e74169147df2d8905995cad351
Austin McGee <austinwm@gmail.com>
2013-04-02 05:15:33 -0700

    Missed a change in a previous commit.

--- mainboard-firmware/src/momo/memory_manager.c
+++ mainboard-firmware/src/momo/memory_manager.c
@@ -12,7 +12,7 @@ typedef struct {
 static inline memory_block create_block( unsigned int subsection_index, unsigned int subsection_length ) {
   memory_block block;
   block.start_address = MEMORY_SUBSECTION_ADDR(subsection_index);
-  block.end_address = MEMORY_SUBSECTION_SIZE*0xFL+block.start_address;
+  block.end_address = MEMORY_SUBSECTION_SIZE*subsection_length+block.start_address;
   return block;
 }
 #define BLOCK_SIZE(block) (block.end_address-block.start_address)
