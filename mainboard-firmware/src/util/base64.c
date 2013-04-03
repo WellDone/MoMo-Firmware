2c928e4fb4b01ca08b38b216667ac602a276e552
Austin McGee <austinwm@gmail.com>
2013-04-03 01:37:48 -0700

    Fix base64 encoding utility.

--- mainboard-firmware/src/util/base64.c
+++ mainboard-firmware/src/util/base64.c
@@ -14,16 +14,18 @@ unsigned int base64_encode( const BYTE* data, unsigned int input_length, char* e
 {
   unsigned int output_length;
   int i,j;
+  BYTE octet_a, octet_b, octet_c;
+  unsigned long triple;
   output_length = 4 * ((input_length + 2) / 3);
-  if ( output_length < out_size )
+  if ( output_length > out_size )
     return 0;
 
   for (i = 0, j = 0; i < input_length;) {
-      unsigned long octet_a = i < input_length ? data[i++] : 0;
-      unsigned long octet_b = i < input_length ? data[i++] : 0;
-      unsigned long octet_c = i < input_length ? data[i++] : 0;
+      octet_a = data[i++];
+      octet_b = i < input_length ? data[i++] : 0;
+      octet_c = i < input_length ? data[i++] : 0;
 
-      unsigned long triple = (octet_a << 0x10) + (octet_b << 0x08) + octet_c;
+      triple = (octet_a << 0x10) + (octet_b << 0x08) + octet_c;
 
       encoded_data[j++] = encoding_table[(triple >> 3 * 6) & 0x3F];
       encoded_data[j++] = encoding_table[(triple >> 2 * 6) & 0x3F];
