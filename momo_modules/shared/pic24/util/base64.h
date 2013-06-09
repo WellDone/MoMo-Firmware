c9971e4eef19d9c2d244026ce7753ec18cb2b2c2
Austin McGee <austinwm@gmail.com>
2013-06-08 23:11:26 -0700

    Generalize more!

--- momo_modules/shared/pic24/util/base64.h
+++ momo_modules/shared/pic24/util/base64.h
@@ -1,7 +1,7 @@
 #ifndef __base64_h__
 #define __base64_h__
 
-#include "common.h"
+#include "common_types.h"
 
 unsigned int base64_encode( const BYTE* data, unsigned int input_length, char* encoded_data, unsigned int out_size );
 
