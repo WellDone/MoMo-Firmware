f8016b392258b12ee71c6831f78aeb1a79216bc8
dclarino <davidbclarino@gmail.com>
2013-07-30 20:35:09 -0700

    Failing rake test:all in momo_modules/shared/pic24 environment
    build/test/mocks/mock_bus_slave.h:17:53: error: expected ')' before 'cmock_line'

---
+++ momo_modules/shared/pic24/test/test_test.c
@@ -0,0 +1,17 @@
+#include "mock_bus_slave.h"
+#include "mock_bus.h"
+#include "test.h"
+#include <string.h>
+#include <stdlib.h>
+#include "mock_protocol.h" 
+#include "unity.h"
+
+void setUp(void) {
+
+}
+void tearDown(void) {
+
+}
+void test_test_command(void) {
+    TEST_ASSERT_EQUAL_INT(1, 1);
+}
