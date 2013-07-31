cf197b4800c6f9ae4bbcae7f529e98e0efc916c2
Austin McGee <austinwm@gmail.com>
2013-07-30 21:37:36 -0700

    Some test infrastructure working.

--- momo_modules/shared/pic24/test/test_test.c
+++ momo_modules/shared/pic24/test/test_test.c
@@ -1,9 +1,9 @@
-#include "mock_bus_slave.h"
-#include "mock_bus.h"
-#include "test.h"
-#include <string.h>
-#include <stdlib.h>
-#include "mock_protocol.h" 
+//#include "mock_bus_slave.h"
+//#include "mock_bus.h"
+//#include "test.h"
+//#include <string.h>
+//#include <stdlib.h>
+//#include "mock_protocol.h" 
 #include "unity.h"
 
 void setUp(void) {
@@ -13,5 +13,5 @@ void tearDown(void) {
 
 }
 void test_test_command(void) {
-    TEST_ASSERT_EQUAL_INT(1, 1);
+    //TEST_ASSERT_EQUAL_INT(1, 1);
 }
