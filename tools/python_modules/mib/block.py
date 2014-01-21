cf58e8bee538e1ce484373e6bd6a84d1623f7d15
Tim Burke <timburke@alumni.rice.edu>
2014-01-20 21:19:03 -0800

    working version of MIB Block dumping and creating code

--- tools/python_modules/mib/block.py
+++ tools/python_modules/mib/block.py
@@ -1,6 +1,7 @@
 #block.py
 #An object representing a MIBBlock
 
+from handler import MIBHandler
 import sys
 import os.path
 sys.path.append(os.path.join(os.path.dirname(__file__), '..'))
@@ -23,10 +24,13 @@ magic_offset = 15
 
 class MIBBlock:
 	"""
-	The block in program memory describing a MoMo application module.
+	The block in program memory describing a MoMo application module.  The MIB block
+	contains information on the application module and a sparse matrix representation
+	of a jump table containing all of the feature, command combinations that the 
+	module knows how to respond to.
 	"""
 
-	def __init__(self, ih):
+	def __init__(self, ih=None):
 		"""
 		Given an intelhex object, extract the MIB block information
 		from it or raise an exception if a MIBBlock cannot be found
@@ -37,10 +41,50 @@ class MIBBlock:
 
 		self.base_addr = build_settings["mib12"]["mib"]["base_address"]
 		self.curr_version = build_settings["mib12"]["mib"]["current_version"]
+		self.features = {}
+		self.valid = True
+		self.error_msg = ""
+		self.num_features = 0
 
-		self._load_from_hex(ih)
+		if ih is not None:
+			try:
+				self._load_from_hex(ih)
+				self._check_consistency()
+				self._interpret_handlers()
+				self._build_feature_map()
+			except ValueError as e:
+				self.error_msg = e
+				self.valid = False
 
-		self._check_consistency()
+	def add_command(self, feature, cmd, handler):
+		"""
+		Add a command to the MIBBlock.  The cmd must be sequential after
+		the last command in the feature that it is added to, i.e. you must add
+		feature 1, cmd 0 before adding feature 1 cmd 1.
+		"""
+
+		if feature not in self.features:
+			self.features[feature] = []
+			self.num_features = len(self.features.keys())
+
+		if cmd != len(self.features[feature]):
+			raise ValueError("Added noncontiguous command %d (%s) to feature %d, last command was %d" % (cmd, handler.symbol, feature, len(self.features[feature])))
+
+		self.features[feature].append(handler)
+
+	def _interpret_handlers(self):
+		self.handlers = map(lambda i: MIBHandler(self.spec_list[i], address=self.handler_addrs[i]), xrange(0, len(self.spec_list)))
+
+	def _build_feature_map(self):
+		cmd_i  = 0
+
+		self.features = {}
+		for i in xrange(0, len(self.feature_list)):
+			self.features[self.feature_list[i]] = []
+			for j in xrange(self.cmd_list[i], self.cmd_list[i+1]):
+				self.features[self.feature_list[i]].append(self.handlers[cmd_i])
+
+				cmd_i += 1
 
 	def _check_magic(self, ih):
 		magic_addr = self.base_addr + magic_offset
@@ -57,16 +101,21 @@ class MIBBlock:
 		Check that the features, commands, handlers, and specifications are mutually consistent
 		"""
 
-		if len(self.cmds) != len(self.features)+1:
+		self.valid = False
+
+		if self.num_features != len(self.feature_list):
+			raise ValueError("Mismatch between length of feature list and num_features.")
+
+		if len(self.cmd_list) != len(self.feature_list)+1:
 			raise ValueError("Command list has wrong number of entries, was %d, should be %d" % (len(cmds), len(features)+1))
 
-		if len(self.specs) != len(self.handlers):
-			raise ValueError("There should be a 1-1 mapping between command specs and handlers. Their lengths differed (specs: %d, handlers: %d)." % (len(self.specs), len(self.handlers)))
+		if len(self.spec_list) != len(self.handler_addrs):
+			raise ValueError("There should be a 1-1 mapping between command specs and handlers. Their lengths differed (specs: %d, handlers: %d)." % (len(self.specs), len(self.handler_addrs)))
 
-		if len(self.handlers) != self.cmds[-1]:
-			raise ValueError("Command array has invalid format.  Last entry should be the number of total handlers (total handlers: %d, cmd entry: %d)" % (len(self.handlers), self.cmds[-1]))
+		if len(self.handler_addrs) != self.cmd_list[-1]:
+			raise ValueError("Command array has invalid format.  Last entry should be the number of total handlers (total handlers: %d, cmd entry: %d)" % (len(self.handler_addrs), self.cmds[-1]))
 
-		return True
+		self.valid = True
 
 	def _load_from_hex(self, ih):
 		if not self._check_magic(ih):
@@ -78,28 +127,40 @@ class MIBBlock:
 		self.specs_addr = decode_goto(ih, self.base_addr + specs_offset)
 		self.handlers_addr = decode_goto(ih, self.base_addr + handlers_offset)
 
-		self.features = decode_table(ih, self.features_addr, decode_retlw)
-		self.cmds = decode_table(ih, self.cmds_addr, decode_retlw)
-		self.specs = decode_table(ih, self.specs_addr, decode_retlw)
-		self.handlers = decode_table(ih, self.handlers_addr, decode_goto)
+		self.feature_list = decode_table(ih, self.features_addr, decode_retlw)
+		self.cmd_list = decode_table(ih, self.cmds_addr, decode_retlw)
+		self.spec_list = decode_table(ih, self.specs_addr, decode_retlw)
+		self.handler_addrs = decode_table(ih, self.handlers_addr, decode_goto)
 
 		self.name = decode_string(ih, self.base_addr + name_offset, 7)
 		self.hw_type = decode_retlw(ih, self.base_addr + hw_offset)
-		self.info = decode_retlw(ih, self.base_addr + info_offset)
-		self.module_type = decode_retlw(ih, self.base_addr + type_offset)
+		#self.info = decode_retlw(ih, self.base_addr + info_offset)
+		#self.module_type = decode_retlw(ih, self.base_addr + type_offset)
 
+	def __str__(self):
+		rep = "\n"
 
-ih = intelhex.IntelHex16bit('/home/timburke/src/welldone/MoMo-Firmware/momo_modules/gsm_module/build/output/gsm_module_16lf1823.hex')
+		rep += "MIB Block\n"
+		rep += "---------\n"
 
-try:
-	block = MIBBlock(ih)
-except ValueError as e:
-	print e
+		if not block.valid:
+			rep += "Block invalid: %s\n" % block.error_msg
+			return rep
 
-print block.num_features
-print block.features
-print block.cmds
-print block.handlers
-print block.specs
-print block.name
-print block.module_type
+		rep += "Block Valid\n"
+		rep += "Number of Features: %d\n" % block.num_features
+		rep += "Feature List\n"
+
+		for f in block.features.keys():
+			rep += "%d:\n" % f
+			for i,h in enumerate(block.features[f]):
+				rep += "\t%d: %s\n" % (i, str(h))
+
+		return rep
+
+block = MIBBlock()
+block.add_command(3, 0, MIBHandler.Create('_test', 2, 0))
+block.add_command(3, 1, MIBHandler.Create('asdf', 1, True))
+block.add_command(4, 0, MIBHandler.Create('_hello', 3, True))
+
+print str(block)
