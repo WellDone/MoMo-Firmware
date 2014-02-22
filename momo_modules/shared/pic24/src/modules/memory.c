57fce4e86c9b8527e27bef2ab2561b29a8b0d03b
Tim Burke <timburke@alumni.rice.edu>
2014-02-22 12:45:48 -0800

    Update memory spi code for speed

--- momo_modules/shared/pic24/src/modules/memory.c
+++ momo_modules/shared/pic24/src/modules/memory.c
@@ -7,7 +7,6 @@
 #define MEMORY_STATUS_OVERFLOWN SPI1STATbits.SPIROV
 #define MEMORY_RX_STATUS SPI1STATbits.SPIRBF
 #define MEMORY_BUFFER_REGISTER SPI1BUF
-#define TIMEOUT 10000
 
 typedef enum {
   WREN = 0b00000110,
@@ -21,25 +20,27 @@ typedef enum {
   BE   = 0b11000111
 } memory_instructions;
 
-static unsigned short memory_capacity;
+//Functions only for this 
+static BYTE spi_transfer(BYTE data);
+static void spi_send_addr(unsigned long data);
 
-bool shift_out( BYTE data );
+#define spi_receive()   spi_transfer(0x00)
 
-#define WRITE_MODE_ENABLE() shift_out( WREN )
-#define WRITE_MODE_DISABLE() shift_out( WRDI )
-#define READ_IDENTIFICATION() shift_out( RDID )
-#define READ_STATUS_REGISTER() shift_out( RDSR )
-#define PAGE_PROGRAM_MODE() shift_out( PP )
-#define READ_MODE() shift_out( READ )
-#define ERASE_SUBSECTION() shift_out( SSE )
-#define ERASE_ALL() shift_out( BE )
+#define WRITE_MODE_ENABLE() spi_transfer( WREN )
+#define WRITE_MODE_DISABLE() spi_transfer( WRDI )
+#define READ_IDENTIFICATION() spi_transfer( RDID )
+#define READ_STATUS_REGISTER() spi_transfer( RDSR )
+#define PAGE_PROGRAM_MODE() spi_transfer( PP )
+#define READ_MODE() spi_transfer( READ )
+#define ERASE_SUBSECTION() spi_transfer( SSE )
+#define ERASE_ALL() spi_transfer( BE )
 
 void configure_SPI() {
   SPI1CON1bits.MODE16 = 0; //communication is byte-wide
   SPI1CON1bits.MSTEN = 1; //SPI is in master mode
   SPI1CON1bits.CKP = 1; //data is clocked out on high-low transition
-  SPI1STATbits.SPIEN = 1; // Enable
   SPI1STATbits.SPIROV = 0; // Clear the overflow flag.
+  SPI1STATbits.SPIEN = 1; // Enable
 
   TRISBbits.TRISB15 = 0; // SS
   TRISBbits.TRISB14 = 1; // SDI (IO)
@@ -48,171 +49,139 @@ void configure_SPI() {
   TRISBbits.TRISB12 = 0; // SDCK
 
   DISABLE_MEMORY(); //idle state of SS is high
-  mem_test();
 }
 
-static bool shift_impl( BYTE data, BYTE* data_out ) {
-  unsigned short count = 0;
-  while ( MEMORY_TX_STATUS && count<TIMEOUT)
-    ++count;
+static BYTE spi_transfer(BYTE data)
+{
+
+  while(MEMORY_TX_STATUS)
+    ;
+
   MEMORY_BUFFER_REGISTER = data;
-  while ( MEMORY_RX_STATUS == 0 && count<TIMEOUT )
-    ++count;
+  while(MEMORY_RX_STATUS == 0)
+    ;
 
-  if (count==TIMEOUT)
-    return false;
-
-  *data_out = MEMORY_BUFFER_REGISTER;
-  return true;
-}
-bool shift_out( BYTE data ) {
-  return shift_impl( data, &data );
+  return MEMORY_BUFFER_REGISTER;
 }
 
-//Shift_out the lowest num_bytes bytes of data
-//max sizeof(long) bytes, MSB first
-bool shift_n_out( unsigned long data, short num_bytes ) {
-  num_bytes = (num_bytes-1)<<3; //*=8
-  while( num_bytes >= 0 ) {
-    if ( !shift_out( (data>>num_bytes)&0xFF )) {
-      return false;
-    }
-    num_bytes -= 8;
-  }
-  return true;
-}
-
-bool shift_in( BYTE* out ) {
-  return shift_impl( 0x00, out );
+static void spi_send_addr(unsigned long data)
+{
+    data &= MEMORY_ADDRESS_MASK;
+    spi_transfer((data>>16)&0xFF);
+    spi_transfer((data>>8)&0xFF);
+    spi_transfer(data&0xFF);
 }
 
 bool mem_test() {
   BYTE manufacturer_id;
   BYTE memory_type;
+  BYTE memory_capacity;
 
   ENABLE_MEMORY();
 
   READ_IDENTIFICATION();
-  shift_in( &manufacturer_id );
-  shift_in( &memory_type );
-  shift_in( (BYTE*)&memory_capacity );
+  manufacturer_id = spi_receive();
+  memory_type  = spi_receive();
+  memory_capacity = spi_receive();
 
   DISABLE_MEMORY();
 
-  if ( manufacturer_id != 0x20 || memory_type != 0x71 ) { // M25PX80 = 0x20, 0x71
+  if ( manufacturer_id != 0x20 || memory_type != 0x71 || memory_capacity != 0x14 ) // M25PX80 = 0x20, 0x71
     return false;
-  }
 
   return true;
 }
 
-static inline void _impl_mem_status( BYTE* status) {
-  READ_STATUS_REGISTER();
-  shift_in( status );
-}
-static inline void mem_enable_write() {
+static inline void mem_enable_write() 
+{
   ENABLE_MEMORY();
   WRITE_MODE_ENABLE();
   DISABLE_MEMORY();
 }
-static inline void mem_wait_while_writing() {
+
+static inline void mem_wait_while_writing() 
+{
+  BYTE status = 0b1;
+
   ENABLE_MEMORY();
-  BYTE status;
-  _impl_mem_status( &status );
-  while (status & 0b1) {
-    shift_in( &status );
-  }
+  
+  READ_STATUS_REGISTER();
+  while (status & 0b1)
+    status = spi_receive();
+
   DISABLE_MEMORY();
 }
 
 // Length is capped at 256, 1 page of flash memory.
-bool mem_write(unsigned long addr, const BYTE *data, unsigned int length) {
-  int i;
-  bool success = true;
-  if ( length > 256) { //TODO: bitwise-ify
+void mem_write(unsigned long addr, const BYTE *data, unsigned int length) 
+{
+  unsigned int i;
+
+  if ( length > 256)
     length = 256;
-  }
 
   mem_wait_while_writing();
   mem_enable_write();
   ENABLE_MEMORY();
   PAGE_PROGRAM_MODE();
 
-  addr &= MEMORY_ADDRESS_MASK;
-  if ( !shift_n_out( addr, 3 ) ) {
-    success = false;
-  }
+  spi_send_addr(addr);
 
-  if ( success ) {
-    for(i = 0; i < length; ++i) {
-      if (!shift_out( data[i] )) {
-        success = false;
-      }
-    }
-  }
+  for(i = 0; i < length; ++i) 
+    spi_transfer(data[i]);
 
   DISABLE_MEMORY();
-  return success;
 }
 
-bool mem_read(unsigned long addr, BYTE* buf, unsigned int numBytes) {
+void mem_read(unsigned long addr, BYTE* buf, unsigned int numBytes) 
+{
   BYTE* bufEnd = buf+numBytes;
-  bool success = true;
 
   ENABLE_MEMORY();
   READ_MODE();
 
-  addr &= MEMORY_ADDRESS_MASK;
-  if (!shift_n_out( addr, 3 )) {
-    success = false;
-  }
+  spi_send_addr(addr);
 
-  if (success) {
-    while ( buf != bufEnd ) {
-      if (!shift_in( buf )) {
-        success = false;
-        break;
-      }
-      ++buf;
-    }
-  }
-  success = (buf == bufEnd);
+  while ( buf != bufEnd ) 
+    *buf++ = spi_receive();
 
   DISABLE_MEMORY();
-  return success;
 }
 
-BYTE mem_status() {
+BYTE mem_status() 
+{
   BYTE status;
+
   ENABLE_MEMORY();
-  _impl_mem_status( &status );
+  READ_STATUS_REGISTER();
+  status = spi_receive();
   DISABLE_MEMORY();
+
   return status;
 }
 
-unsigned short mem_capacity()
+void mem_clear_all() 
 {
-  return memory_capacity;
-}
-
-void mem_clear_all() {
   mem_wait_while_writing();
   mem_enable_write();
 
   ENABLE_MEMORY();
   ERASE_ALL();
   DISABLE_MEMORY();
+
+  mem_wait_while_writing();
 }
 
-void mem_clear_subsection( unsigned long addr ) 
+//FIXME: This routine will take a long time (for the erase cycle), should it be a synchronous call?
+void mem_clear_subsection(unsigned long addr) 
 {
-  addr &= MEMORY_ADDRESS_MASK;
-
   mem_wait_while_writing();
   mem_enable_write();
 
   ENABLE_MEMORY();
   ERASE_SUBSECTION();
-  shift_n_out( addr, 3 );
+  spi_send_addr(addr);
   DISABLE_MEMORY();
+
+  mem_wait_while_writing();
 }
--- momo_modules/shared/pic24/src/modules/memory.h
+++ momo_modules/shared/pic24/src/modules/memory.h
@@ -2,18 +2,24 @@
 #include <stdlib.h>
 #include "pic24.h"
 
-#define MEMORY_SUBSECTION_MASK 0xFFFL
-#define MEMORY_SUBSECTION_SIZE 0x1000L
+#define MEMORY_SUBSECTION_MASK 0xFFFULL
+#define MEMORY_SUBSECTION_SIZE 0x1000ULL
+
+
 #define MEMORY_SUBSECTION_ADDR(num) (MEMORY_SUBSECTION_SIZE*num)
-#define MEMORY_SUBSECTION_NUM(addr) (addr>>3)
+
+//Convert a sector number to a subsector number (sector size=64K, subsector size=4K, 16 subsectors per sector)
+#define MEMORY_SECTION_TO_SUB(sec)	(((unsigned long long)sec) << 4)
+#define MEMORY_SECTION_ADDR(sec) 	MEMORY_SUBSECTION_ADDR(MEMORY_SECTION_TO_SUB(sec))
+
 #define MEMORY_ADDRESS_MASK 0xFFFFFL
+#define MEMORY_CAPACITY			0x14		//defined in M25PX_80 datasheet (page 21)
 
 void configure_SPI();
 
-bool mem_write( unsigned long addr, const BYTE* data, unsigned int length );
-bool mem_read( unsigned long addr, BYTE* buf, unsigned int numBytes );
+void mem_write( unsigned long addr, const BYTE* data, unsigned int length );
+void mem_read( unsigned long addr, BYTE* buf, unsigned int numBytes );
 void mem_clear_subsection( unsigned long addr );
 void mem_clear_all();
 BYTE mem_status();
-unsigned short mem_capacity();
 bool mem_test();
