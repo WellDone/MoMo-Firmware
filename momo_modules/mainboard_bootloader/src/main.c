69d72baaf74728983db2c362d2f6f728848ccdaf
Tim Burke <timburke@alumni.rice.edu>
2014-04-12 12:41:27 -0700

    Update bootloader to work with new board.
    - now waits for ALARM pin to be released before starting a reflash.
    This fixes a bug with using Pickit3 to program the bootloader where
    the bootloader would trigger immediately after being programmed
    - updated SPI flash to full-speed.  There was no reason to be using
    it at 1/512th speed.
    - communication with main controller firmware is now done not with
    EEPROM but a perisistent global variable.  The new chip has no EEPROM

--- momo_modules/mainboard_bootloader/src/main.c
+++ momo_modules/mainboard_bootloader/src/main.c
@@ -1,132 +1,104 @@
-// FBS
-#pragma config BWRP = OFF               // Table Write Protect Boot (Boot segment may be written)
-#pragma config BSS = OFF                // Boot segment Protect (No boot program Flash segment)
+// CONFIG4
+#pragma config DSWDTPS = DSWDTPS1F      // Deep Sleep Watchdog Timer Postscale Select bits (1:68719476736 (25.7 Days))
+#pragma config DSWDTOSC = LPRC          // DSWDT Reference Clock Select (DSWDT uses LPRC as reference clock)
+#pragma config DSBOREN = ON             // Deep Sleep BOR Enable bit (DSBOR Enabled)
+#pragma config DSWDTEN = OFF            // Deep Sleep Watchdog Timer Enable (DSWDT Disabled)
+#pragma config DSSWEN = ON              // DSEN Bit Enable (Deep Sleep is controlled by the register bit DSEN)
 
-// FGS
-#pragma config GWRP = OFF               // General Segment Code Flash Write Protection bit (General segment may be written)
-#pragma config GCP = OFF                // General Segment Code Flash Code Protection bit (No protection)
+// CONFIG3
+#pragma config WPFP = WPFP63            // Write Protection Flash Page Segment Boundary (Page 52 (0xFC00))
+#pragma config VBTBOR = ON              // VBAT BOR enable bit (VBAT BOR enabled)
+#pragma config SOSCSEL = ON             // SOSC Selection bits (SOSC circuit selected)
+#pragma config WDTWIN = PS75_0          // Watch Dog Timer Window Width (Watch Dog Timer Window Width is 75 percent)
+#pragma config BOREN = ON               // Brown-out Reset Enable (Brown-out Reset Enable)
+#pragma config WPDIS = WPDIS            // Segment Write Protection Disable (Disabled)
+#pragma config WPCFG = WPCFGDIS         // Write Protect Configuration Page Select (Disabled)
+#pragma config WPEND = WPENDMEM         // Segment Write Protection End Page Select (Write Protect from WPFP to the last page of memory)
 
-// FOSCSEL
-#pragma config FNOSC = FRC           	// Oscillator Select (8 MHz FRC oscillator (FRC)) with PLL
-#pragma config IESO  = OFF              // Internal External Switch Over bit (Internal External Switchover mode disabled (Two-Speed Start-up disabled))
+// CONFIG2
+#pragma config POSCMD = NONE            // Primary Oscillator Select (Primary Oscillator Disabled)
+#pragma config BOREN1 = EN              // BOR Override bit (BOR Enabled [When BOREN=1])
+#pragma config IOL1WAY = OFF            // IOLOCK One-Way Set Enable bit (The IOLOCK bit can be set and cleared using the unlock sequence)
+#pragma config OSCIOFNC = ON            // OSCO Pin Configuration (OSCO/CLKO/RC15 functions as port I/O (RC15))
+#pragma config FCKSM = CSDCMD           // Clock Switching and Fail-Safe Clock Monitor Configuration bits (Clock switching and Fail-Safe Clock Monitor are disabled)
+#pragma config FNOSC = FRC              // Initial Oscillator Select (Fast RC Oscillator (FRC))
+#pragma config ALTVREF = DLT_AV_DLT_CV  // Alternate VREF/CVREF Pins Selection bit (Voltage reference input, ADC =RA9/RA10 Comparator =RA9,RA10)
+#pragma config IESO = OFF               // Internal External Switchover (Disabled)
 
-// FOSC
-#pragma config POSCMOD = NONE           // Primary Oscillator Configuration bits (Primary oscillator disabled)
-#pragma config OSCIOFNC = OFF            // CLKO Enable Configuration bit (CLKO output signal is active on the OSCO pin)
-#pragma config POSCFREQ = HS            // Primary Oscillator Frequency Range Configuration bits (Primary oscillator/external clock input frequency greater than 8 MHz)
-#pragma config SOSCSEL = SOSCHP         // SOSC Power Selection Configuration bits (Secondary oscillator configured for high-power operation)
-#pragma config FCKSM = CSECMD           // Clock Switching and Monitor Selection (Clock switching is enabled, Fail-Safe Clock Monitor is disabled)
-
-// FWDT
-#pragma config WDTPS = PS32768          // Watchdog Timer Postscale Select bits (1:32,768)
-#pragma config FWPSA = PR128            // WDT Prescaler (WDT prescaler ratio of 1:128)
-#pragma config WINDIS = OFF             // Windowed Watchdog Timer Disable bit (Standard WDT selected; windowed WDT disabled)
-#pragma config FWDTEN = OFF             // Watchdog Timer Enable bit (WDT disabled (control is placed on the SWDTEN bit))
-
-// FPOR
-#pragma config BOREN = BOR0             // Brown-out Reset Enable bits (Brown-out Reset disabled in hardware; SBOREN bit disabled)
-#pragma config PWRTEN = ON              // Power-up Timer Enable bit (PWRT enabled)
-#pragma config I2C1SEL = PRI            // Alternate I2C1 Pin Mapping bit (Default location for SCL1/SDA1 pins)
-#pragma config BORV = V18               // Brown-out Reset Voltage bits (Brown-out Reset set to lowest voltage (1.8V))
-#pragma config MCLRE = ON               // MCLR Pin Enable bit (RA5 input pin disabled; MCLR enabled)
-
-// FICD
-#pragma config ICS = PGx1               // ICD Pin Placement Select bits (PGC1/PGD1 are used for programming and debugging the device)
-
-// FDS
-#pragma config DSWDTPS = DSWDTPSF       // Deep Sleep Watchdog Timer Postscale Select bits (1:2,147,483,648 (25.7 Days))
-#pragma config DSWDTOSC = LPRC          // DSWDT Reference Clock Select bit (DSWDT uses LPRC as reference clock)
-#pragma config RTCOSC = LPRC            // RTCC Reference Clock Select bit (RTCC uses LPRC as reference clock)
-#pragma config DSBOREN = ON             // Deep Sleep Zero-Power BOR Enable bit (Deep Sleep BOR enabled in Deep Sleep)
-#pragma config DSWDTEN = OFF            // Deep Sleep Watchdog Timer Enable bit (DSWDT disabled)
+// CONFIG1
+#pragma config WDTPS = PS32768          // Watchdog Timer Postscaler Select (1:32,768)
+#pragma config FWPSA = PR128            // WDT Prescaler Ratio Select (1:128)
+#pragma config FWDTEN = WDT_SW          // Watchdog Timer Enable (WDT controlled with the SWDTEN bit)
+#pragma config WINDIS = OFF             // Windowed WDT Disable (Standard Watchdog Timer)
+#pragma config ICS = PGx1               // Emulator Pin Placement Select bits (Emulator functions are shared with PGEC1/PGED1)
+#pragma config LPCFG = OFF              // Low power regulator control (Disabled)
+#pragma config GWRP = OFF               // General Segment Write Protect (Disabled)
+#pragma config GCP = OFF                // General Segment Code Protect (Code protection is disabled)
+#pragma config JTAGEN = OFF             // JTAG Port Enable (Disabled)
 
 #include "pic24.h"
+#include "ioport.h"
 #include "memory.h"
 #include "bootloader.h"
 #include "constants.h"
 
-#define kEEPROMPage 0x7F
-#define kEEPROMOffset 0xFE00		//valid for pic24f16ka101
-
-enum
-{
-	kEEPROMEraseWord = 0x4058,
-	kEEPROMWriteWord = 0x4004
-};
-
-void eeprom_erase(unsigned int offset);
-unsigned int eeprom_read(unsigned int offset);
+unsigned int _BOOTLOADER_VAR reflash __attribute__((persistent));
 
 int _BOOTLOADER_CODE main(void)
 {
-	AD1PCFG = 0xFFFF;
-	ALARMTRIS = 1;
+	DIR(ALARM) = INPUT;
 
 	//Allow 100 ms for someone to assert the alarm pin in case this is a
 	//power on reset.
 	DELAY_MS(100);
 
-	//If alarm pin is low for 400 ms, load backup firmware
-	if (ALARMPIN == 0)
+	//If alarm pin and data pin are low for 400 ms, load backup firmware
+	if (PIN(ALARM) == 0)
 	{
 		DELAY_MS(400);
-		if (ALARMPIN == 0)
+		if (PIN(ALARM) == 0)
 		{
+			//Wait for the pin to go high, then pull it low and start the reflash
+			//This makes sure that if there is just something that pulls alarm low
+			//on boot, we don't start reflashing ourselves.
+			while(PIN(ALARM) == 0)
+				;
+
 			//Reflash ourselves and pull alarm pin low during the operation so
 			//people can know when we're done.
-			ALARMPIN = 0;
-			ALARMTRIS= 0;
+			LAT(ALARM) = 0;
+			DIR(ALARM) = OUTPUT;
+
+			DELAY_MS(100);
+
 			program_application(kBackupFirmwareSector);
-			ALARMTRIS = 1;
+			DIR(ALARM) = INPUT;
 		}
 	}
-	else if (eeprom_read(kBootloaderMagicEEPROMLoc) == 0xAA)
+	else if (reflash == kReflashMagic)
 	{
 		//If we are told to reflash, do so.
-		ALARMPIN = 0;
-		ALARMTRIS= 0;
+		reflash = 0x0000;
+		LAT(ALARM) = 0;
+		DIR(ALARM) = OUTPUT;
 		program_application(kMainFirmwareSector);
-		eeprom_erase(kBootloaderMagicEEPROMLoc);
-		ALARMTRIS = 1;
+		DIR(ALARM) = INPUT;
 	}
 
 	//TODO: this only works if I jump directly to the start of the code, rather than the
 	//goto instruction stored at 0x100.  It is unclear why but it also happens when I 
 	//directly program the chip to jump to 0x100 on reset with a goto instruction at 0x100
 	//that points to 0x200
+	
 	if (valid_instruction(0x100))
 		goto_address(0x200);
 
-	ALARMPIN = 0;
 	while(true)
 	{
-		ALARMTRIS= 0;
-		DELAY_MS(250);
-		ALARMTRIS=1;
-		DELAY_MS(250);
+		LAT(ALARM) = 0;
+		DIR(ALARM) = ~ DIR(ALARM);
+		DELAY_MS(250)
 	}
 
 	return 0;
-}
-
-void _BOOTLOADER_CODE eeprom_erase(unsigned int offset)
-{
-	NVMCON = kEEPROMEraseWord;
-	TBLPAG = kEEPROMPage;
-
-	offset |= kEEPROMOffset;
-	__builtin_tblwtl(offset, 0);
-	asm volatile ("disi #5");
-	__builtin_write_NVM();
-
-	while(_WR)
-		;
-}
-
-unsigned int _BOOTLOADER_CODE eeprom_read(unsigned int offset)
-{
-	TBLPAG = kEEPROMPage;
-	offset |= kEEPROMOffset;
-
-	return __builtin_tblrdl(offset);
 }
