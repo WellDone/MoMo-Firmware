5d10b3cfeac9674ca57cb02a50c6c41a99974ea2
Austin McGee <austinwm@gmail.com>
2014-04-12 20:44:02 -0700

    And some more files...

--- tools/bin/momosensor.py
+++ tools/bin/momosensor.py
@@ -29,10 +29,51 @@ class MoMoSensor(cmdln.Cmdln):
 		def log_event( scheduler ):
 			s.enter( .1, 1, log_event, (scheduler,) )
 			value = randint(1,1000)
-			con.log_sensor_event( int(sensor_address), int(meta), value )
+			con.sensor_log( int(sensor_address), int(meta), value )
 			print "Logged event!  Value: %d" % value
 
 		s.enter( 1, 1, log_event, (s,) )
+		s.run()
+
+	@cmdln.option('-p', '--port', help='Serial port that fsu is plugged into')
+	def do_test(self, subcmd, opts):
+		"""${cmd_name}: Test subsection bound overflow
+
+		${cmd_usage}
+		${cmd_option_list}
+		"""
+		
+		con = self._get_controller(opts)
+
+		meta = 0x0;
+		s = sched.scheduler(time.time, time.sleep)
+		def log_event( scheduler, count ):
+			if count >= 342*2 + 10: # = maximum event count, 12 sectors w/ 16 subsectors each (1 for metadata), 12-byte events, 4096-byte subsectors
+				print "COMPLETE"
+				debug = con.sensor_log_debug()
+				if debug.start = debug.min + 4096:
+					print "SUCCESS"
+				else:
+					print "FAILURE"
+				print "DEBUG ADDRESSES: MIN %d, MAX %d, START %d, END %d" % con.sensor_log_debug()
+				return
+				
+			modcount = (count % (343)) + ( count / 343 )
+			if ( modcount != con.sensor_log_count() ):
+				print "FAILURE"
+				print "count: %d, expected: %d" % (con.sensor_log_count(), modcount)
+				print "DEBUG ADDRESSES: MIN %d, MAX %d, START %d, END %d" % con.sensor_log_debug()
+				return
+
+			value = randint(1,1000)
+			s.enter( .01, 1, log_event, (scheduler,count+1) )
+
+			con.sensor_log( 42, int(meta), value )
+			print "(%d,%d) Logged event!  Value: %d" % ((count+1)/343,(count+1)%343, value)
+
+		con.sensor_log_clear()
+		print "All sensor events cleared from PIC memory."
+		s.enter( .1, 1, log_event, (s,0) )
 		s.run()
 
 	@cmdln.option('-p', '--port', help='Serial port that fsu is plugged into')
@@ -44,7 +85,7 @@ class MoMoSensor(cmdln.Cmdln):
 		"""
 		
 		con = self._get_controller(opts)
-		event = con.read_sensor_value()
+		event = con.sensor_log_read()
 
 		print "Stream: %d" % event.stream
 		print "MetaData: %d" % event.metadata
@@ -53,14 +94,48 @@ class MoMoSensor(cmdln.Cmdln):
 
 	@cmdln.option('-p', '--port', help='Serial port that fsu is plugged into')
 	def do_log(self, subcmd, opts, sensor_address, meta, value):
-		"""${cmd_name}: Log one integer value (max: 2^64)
+		"""${cmd_name}: Log one integer value (32-bit)
 
 		${cmd_usage}
 		${cmd_option_list}
 		"""
 		con = self._get_controller(opts)
-		con.log_sensor_event( int(sensor_address), int(meta), int(value) )
+		con.sensor_log( int(sensor_address), int(meta), int(value) )
 		print "Logged event!"
+
+	@cmdln.option('-p', '--port', help='Serial port that fsu is plugged into')
+	def do_count(self, subcmd, opts):
+		"""${cmd_name}: Number of events in the sensor log.
+
+		${cmd_usage}
+		${cmd_option_list}
+		"""
+		con = self._get_controller(opts)
+		print con.sensor_log_count();
+
+	@cmdln.option('-p', '--port', help='Serial port that fsu is plugged into')
+	def do_clear(self, subcmd, opts):
+		"""${cmd_name}: Clear all values from the sensor log, resetting the pointers to
+		  the beginning of the flash_queue.
+
+		${cmd_usage}
+		${cmd_option_list}
+		"""
+		con = self._get_controller(opts)
+		con.sensor_log_clear();
+		print "All sensor events cleared from PIC memory."
+
+	@cmdln.option('-p', '--port', help='Serial port that fsu is plugged into')
+	def do_debug(self, subcmd, opts):
+		"""${cmd_name}: Print out the min, max, start, and end addresses
+
+		${cmd_usage}
+		${cmd_option_list}
+		"""
+		
+		con = self._get_controller(opts)
+
+		print "DEBUG ADDRESSES: MIN %d, MAX %d, START %d, END %d" % con.sensor_log_debug()
 
 	def _get_controller(self, opts):
 		try:
