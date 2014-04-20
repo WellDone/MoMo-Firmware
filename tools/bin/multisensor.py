05a55f3e5c1b29978c4b0ead798b1975dd40216d
Tim Burke <timburke@alumni.rice.edu>
2014-04-20 10:34:06 -0700

    Add pulse counting ability to multisensor

--- tools/bin/multisensor.py
+++ tools/bin/multisensor.py
@@ -1,6 +1,7 @@
 #!/usr/bin/env python
 
 import sys, os
+from time import sleep
 
 sys.path.append(os.path.join(os.path.dirname(__file__), '..'))
 
@@ -16,6 +17,7 @@ class SensorTool(cmdln.Cmdln):
 	@cmdln.option('-p', '--port', help='Serial port that fsu is plugged into')
 	@cmdln.option('-a', '--address', help='The MIB address of the multisensor module' )
 	@cmdln.option('-n', '--average', type=int, default=1, help='Average for N samples' )
+	@cmdln.option('-c', '--continuous', action='store_true', default=False, help='Continually sample and report' )
 	def do_read(self, subcmd, opts):
 		"""${cmd_name}: Read the voltage from the multisensor module
 
@@ -24,9 +26,13 @@ class SensorTool(cmdln.Cmdln):
 		"""
 
 		sens = self._create_proxy(opts)
-		v = self._average(sens, opts.average)
 
-		print "Voltage: %d" % v
+		while True:
+			v = self._average(sens, opts.average)
+			print "Voltage: %d" % v
+
+			if not opts.continuous:
+				break
 
 	@cmdln.option('-p', '--port', help='Serial port that fsu is plugged into')
 	@cmdln.option('-a', '--address', help='The MIB address of the multisensor module' )
@@ -44,6 +50,30 @@ class SensorTool(cmdln.Cmdln):
 		sens = self._create_proxy(opts)
 		sens.set_parameter(name, value)
 
+	@cmdln.option('-p', '--port', help='Serial port that fsu is plugged into')
+	@cmdln.option('-a', '--address', help='The MIB address of the multisensor module' )
+	@cmdln.option('-c', '--continuous', action='store_true', default=False, help='Continually sample and report' )
+	def do_pulse(self, subcmd, opts):
+		"""${cmd_name}: Set the parameter 'name' to the value 'value'
+
+		Valid parameter names are: offset, gain1, gain2, select, invert and delay
+		Valid values are: offset: [0, 255], gain1: [0,127], gain2: [0,7], 
+		select: [current|differential], invert: [yes|no], delay: [1, 255]
+
+		${cmd_usage}
+		${cmd_option_list}
+		"""
+
+		sens = self._create_proxy(opts)
+
+		while True:
+			sens.acquire_pulses()
+			sleep(0.6)
+			val = sens.read_pulses()
+			print "Pulse Count: %d (%d per second)" % (val, 2*val)
+
+			if not opts.continuous:
+				break
 
 	def _create_proxy(self,opts):
 		try:
