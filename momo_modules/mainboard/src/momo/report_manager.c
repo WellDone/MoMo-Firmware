d06ea748e688744aa57dabde34e487aa6713d9cb
Austin McGee <austinwm@gmail.com>
2014-05-29 03:13:02 +0300

    Re-add commented out line (for multiple registered comm modules)

--- momo_modules/mainboard/src/momo/report_manager.c
+++ momo_modules/mainboard/src/momo/report_manager.c
@@ -329,7 +329,7 @@ void receive_gsm_stream_response(unsigned char a)
       char buf[4];
       CRITICAL_LOG( buf, itoa_small( buf, 4, a ) );
     }
-    //taskloop_add( next_comm_module, NULL );
+    taskloop_add( next_comm_module, NULL );
   }
   else
   {
