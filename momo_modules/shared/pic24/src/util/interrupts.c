1904ed59313e6692ba05fb67140474da129070b7
Tim Burke <timburke@alumni.rice.edu>
2014-06-09 18:03:26 -0700

    Add interrupt protection to flash memory routines
    Flash routines should not be interrupted during the time
    that they are writing to the flash RAM.  This could cause
    timing issues if a command is only partially sent and then
    interrupted.

--- momo_modules/shared/pic24/src/util/interrupts.c
+++ momo_modules/shared/pic24/src/util/interrupts.c
@@ -5,3 +5,16 @@ void configure_interrupts()
     _NSTDIS = 0; //Allow interrupt nesting
     _ALTIVT = 0;
 }
+
+int  disable_interrupts()
+{
+	int old_ipl;
+
+	SET_AND_SAVE_CPU_IPL(old_ipl, kMaxInterruptLevel);
+	return old_ipl;
+}
+
+void enable_interrupts(int old_level)
+{
+	RESTORE_CPU_IPL(old_level);
+}
