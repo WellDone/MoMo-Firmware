dc22388f10951e9740d268b8437f1c51ba3cdf2b
Tim Burke <timburke@alumni.rice.edu>
2014-06-25 20:06:27 -0700

    Update performance counting module to keep min, max, count and avg
    Average is an exponentially filtered running average over an 8
    element window so it should converge to the average runtime of
    the last 8 or so profiling event.
    
    Support for this has been added to the momo tool.  Just use
    ```shell
    > momo controller perf_counter <NUMBER>
    ```

--- momo_modules/shared/pic24/src/modules/perf.c
+++ momo_modules/shared/pic24/src/modules/perf.c
@@ -7,6 +7,8 @@ profiler_data	profiler;
 
 void perf_enable_profiling()
 {
+	int i;
+
 	T2CONbits.TSIDL = 0;
 	T2CONbits.TGATE = 0;
 	T2CONbits.TCKPS = 0b00; //1:1 prescaling
@@ -17,29 +19,85 @@ void perf_enable_profiling()
 	TMR3 = 0;
 
 	T2CONbits.TON = 1;
+
+	for (i=0; i<kNumPerformanceCounters; ++i)
+		perf_init_counter((PerformanceCounter)i);
 }
 
 void perf_start_profile(PerformanceCounter counter)
 {
 	if (counter < kNumPerformanceCounters)
-		profiler.counters[counter] = (((uint32)TMR3) << 16) | TMR2;
+		profiler.counters[counter].accum = (((uint32)TMR3) << 16) | TMR2;
 }
 
 void perf_end_profile(PerformanceCounter counter)
 {
 	uint32 diff = (((uint32)TMR3) << 16) | TMR2;
-	if (counter < kNumPerformanceCounters)
-	{
-		if (diff > profiler.counters[counter])
-			diff -= profiler.counters[counter];
-		else
-			diff = 0xFFFFFFFFULL - diff + profiler.counters[counter];
+	
+	if (diff > profiler.counters[counter].accum)
+		diff -= profiler.counters[counter].accum;
+	else
+		diff = 0xFFFFFFFFULL - diff + profiler.counters[counter].accum;
 
-		profiler.counters[counter] = diff;
-	}
+	perf_update_counter(counter, diff);
 }
 
-uint32 perf_get_counter(PerformanceCounter counter)
+void perf_init_counter(PerformanceCounter counter_index)
 {
-	return profiler.counters[counter];
+	performance_counter *c;
+
+	if (counter_index >= kNumPerformanceCounters)
+		return;
+
+	c = &profiler.counters[counter_index];
+
+	c->min = 0xFFFFFFFFULL;
+	c->max = 0;
+	c->mean = 0;
+	c->count = 0;
+	c->accum = 0;
+}
+
+void perf_update_counter(PerformanceCounter counter_index, uint32 value)
+{
+	performance_counter *c;
+
+	if (counter_index >= kNumPerformanceCounters)
+		return;
+
+	c = &profiler.counters[counter_index];
+	++c->count;
+
+	if (value < c->min)
+		c->min = value;
+
+	if (value > c->max)
+		c->max = value;
+
+	c->mean = (c->mean*7 + value) >> 3;
+}
+
+uint32 perf_get_mean(PerformanceCounter counter)
+{
+	return profiler.counters[counter].mean;
+}
+
+uint32 perf_get_max(PerformanceCounter counter)
+{
+	return profiler.counters[counter].max;
+}
+
+uint32 perf_get_min(PerformanceCounter counter)
+{
+	return profiler.counters[counter].min;
+}
+
+uint32 perf_get_count(PerformanceCounter counter)
+{
+	return profiler.counters[counter].count;
+}
+
+const performance_counter * perf_get_counter(PerformanceCounter counter)
+{
+	return &profiler.counters[counter];
 }
