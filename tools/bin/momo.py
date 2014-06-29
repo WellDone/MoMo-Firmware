bd831b8dca08fb17f3bfc4afc74c48b56298979b
Tim Burke <timburke@alumni.rice.edu>
2014-06-29 16:25:31 -0700

    Update shell with new builtins, fix parameter handling
    - can now navigate up the context chain
    - new ArgumentError for invalid arguments
    - better handling of combined kw and positional arguments

--- tools/bin/momo.py
+++ tools/bin/momo.py
@@ -11,6 +11,8 @@ from pymomo.utilities.typedargs import annotate
 from pymomo.commander.meta import initialization
 from pymomo.hex.hexfile import HexFile
 
+builtins = ['help', 'back', 'quit']
+
 @annotate.context("root")
 class InitialContext(dict):
 	pass
@@ -30,17 +32,24 @@ except NotFoundError as e:
 	print "NotFoundError: %s" % str(e)
 except ValidationError as e:
 	print "ValidationError: %s" % str(e)
+except ArgumentError as e:
+	print "ArgumentError: %s" % str(e)
 
-#Setup file path completion
+#Setup file path and function name completion
 import readline, glob
 def complete(text, state):
-    return (glob.glob(os.path.expanduser(text)+'*')+[None])[state]
+	buf = readline.get_line_buffer()
+	if buf.startswith('help ') or " " not in buf:
+		funcs = annotate.find_all(contexts[-1]).keys() + builtins
+		return filter(lambda x: x.startswith(text), funcs)[state]
+
+	return (glob.glob(os.path.expanduser(text)+'*')+[None])[state]
 
 readline.set_completer_delims(' \t\n;')
 readline.parse_and_bind("tab: complete")
 readline.set_completer(complete)
 
-#If we ended the initial command 
+#If we ended the initial command with a context, start a shell
 if not finished:
 	try:
 		while True:
@@ -54,5 +63,11 @@ if not finished:
 				print "NotFoundError: %s" % str(e)
 			except ValidationError as e:
 				print "ValidationError: %s" % str(e)
+			except ArgumentError as e:
+				print "ArgumentError: %s" % str(e)
+
+			if len(contexts) == 0:
+				sys.exit(0)
+
 	except EOFError:
 		print ""
