50303f3a5b932d4219cf39a87ed057593ba7bbd7
Tim Burke <timburke@alumni.rice.edu>
2015-01-10 19:31:43 -0800

    Update pic12_executive to work with new ioc code
    - supports port B on chips where port B is the IOC port

--- momo_modules/pic12_executive/src/main.c
+++ momo_modules/pic12_executive/src/main.c
@@ -26,14 +26,14 @@ extern void restore_status();
 void interrupt service_isr() {
     //Check if an alarm occurred and reset if so.
     #ifndef __PIC16LF1847__  
-    if(ioc_flag(ALARMIOC))
+    if(ioc_flag_a(ALARMIOC))
     {
         //software debounce the pin to add noise immunity
         __delay_us(10);
         if (PIN(ALARM) == 0)
             exec_reset();
 
-        ioc_flag(ALARMIOC) = 0;
+        ioc_flag_a(ALARMIOC) = 0;
     }
     #endif
 
@@ -132,8 +132,8 @@ void initialize()
 
     /* Make sure that the alarm pin is configured to notify us if the alarm line is ever pulled low */
     #ifndef __PIC16LF1847__
-    ioc_detect_falling(ALARMIOC, 1);
-    ioc_enable();
+    ioc_detect_falling_a(ALARMIOC, 1);
+    ioc_enable_a();
     #endif
 
     /* Set all PORTA pins to be digital I/O (instead of analog input). */
