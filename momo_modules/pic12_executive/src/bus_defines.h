fdf34c54da6a3f5eb2390a55f4018000294adddd
Tim Burke <timburke@alumni.rice.edu>
2015-03-17 18:56:31 -0700

    Remove unused definitions

--- momo_modules/pic12_executive/src/bus_defines.h
+++ momo_modules/pic12_executive/src/bus_defines.h
@@ -1,12 +1,6 @@
 #ifndef __bus_defines_h__
 #define __bus_defines_h__
 
-//Offsets from mib_data to the three overlapping buffers that store
-//the command packet, return status and return value.
-#define kCommandOffset 0
-#define kReturnStatusOffset 1
-#define kReturnValueOffset 3
-
 //Bus error codes that can be returned
 #define kNoMIBError 				0
 #define kUnsupportedCommand 		1
